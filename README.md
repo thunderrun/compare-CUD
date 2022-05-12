5ca3369f625c9e2415048578f1397bca970fbdb2
yun.zou@amh-group.com <yun.zou@amh-group.com>
2022-05-12 14:28:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# compare-CUD
+
+Save and restore window size and position
+
+## Usage
+
+```bash
+npm i 'save-window-size-position'
+```
+
+```js
+// window.html
+import { saveWindowStateBeforeUnload, restoreWindowState } from 'save-window-size-position'
+
+restoreWindowState();
+saveWindowStateBeforeUnload();
+```
+
+or
+
+```js
+// parent.html
+import { getSavedWindowState } from 'save-window-size-position';
+
+window.open('./test.html', undefined, `popup=1,${getSavedWindowState()}`);
+// won't resize the window on open, but width and height are inaccurate (not only if the user zoomed)
+```
+
+```js
+// window.html
+import { saveWindowStateBeforeUnload } from 'save-window-size-position'
+
+// restoreWindowState(); // resize to make the window size accurate
+saveWindowStateBeforeUnload();
+```
+
+### Parameters
+
+```js
+restoreWindowState(
+  defaultOuterWidth, 
+  defaultOuterHeight, 
+  defaultScreenX, 
+  defaultScreenY,
+);
+
+getSavedWindowState(
+  defaultInnerWidth, 
+  defaultInnerHeight, 
+  defaultScreenX, 
+  defaultScreenY,
+);
+```
+
---
+++ index.js
@@ -0,0 +1,77 @@
+const saveWindowStateBeforeUnload = () => {
+
+  window.onbeforeunload = function () {
+
+    const lastWindowState = {
+      outerHeight: window.outerHeight,
+      outerWidth: window.outerWidth,
+      innerWidth: window.innerWidth,
+      innerHeight: window.innerHeight,
+      screenX: window.screenX,
+      screenY: window.screenY,
+    }
+
+    localStorage.setItem('lastWindowState', JSON.stringify(lastWindowState));
+
+    return null;
+  };
+};
+
+const restoreWindowState = (defaultOuterWidth, defaultOuterHeight, defaultScreenX, defaultScreenY) => {
+
+  // resize and reposition window
+  let lastWindowState = JSON.parse(localStorage.getItem('lastWindowState'))
+
+  if (lastWindowState === null) {
+    if (defaultOuterWidth || defaultOuterHeight || defaultScreenX || defaultScreenY) {
+      lastWindowState = {}
+    } else {
+      return;
+    }
+  }
+
+  const outerHeight = lastWindowState.outerHeight || defaultOuterHeight;
+  const outerWidth = lastWindowState.outerWidth || defaultOuterWidth;
+  const screenX = lastWindowState.screenX || defaultScreenX;
+  const screenY = lastWindowState.screenY || defaultScreenY;
+
+  window.resizeTo(
+    outerWidth,
+    outerHeight,
+  );
+
+  window.moveTo(screenX, screenY);
+
+};
+
+const getSavedWindowState = (defaultInnerWidth, defaultInnerHeight, defaultScreenX, defaultScreenY) => {
+
+  const lastWindowState = JSON.parse(localStorage.getItem('lastWindowState'));
+
+  if (!lastWindowState) {
+    let windowFeatures = '';
+    if (defaultInnerWidth) {
+      windowFeatures += `innerWidth=${defaultInnerWidth},`
+    }
+    if (defaultInnerHeight) {
+      windowFeatures += `innerHeight=${defaultInnerHeight},`
+    }
+    if (defaultScreenX) {
+      windowFeatures += `screenX=${defaultScreenX},`
+    }
+    if (defaultScreenY) {
+      windowFeatures += `screenY=${defaultScreenY},`
+    }
+    return windowFeatures;
+  }
+
+  const innerHeight = lastWindowState.innerHeight;
+  const innerWidth = lastWindowState.innerWidth;
+  const screenX = lastWindowState.screenX;
+  const screenY = lastWindowState.screenY;
+
+  return `screenX=${screenX},screenY=${screenY},innerWidth=${innerWidth},innerHeight=${innerHeight},`;
+
+};
+
+export { saveWindowStateBeforeUnload, getSavedWindowState, restoreWindowState };
